3d823773ac73eeac498cef12a8e4763a1e3fefce
Lucas Mendes <lucas.lmp77@gmail.com>
2025-05-17 12:07:19 -0300

    prisma inital config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  providers: [AppService, PrismaService],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
