521740f3cf1d28c2d6999b71267b51aeaa6703ab
renan <renanmontanopaz@outlook.com>
2025-06-19 20:02:46 -0300

    pipeline

--- k8s/main.tf
+++ k8s/main.tf
@@ -1,5 +1,4 @@
-# O provedor do Kubernetes. Ele será configurado automaticamente pela pipeline
-# que já terá as credenciais do cluster criado no passo anterior.
+# O provedor do Kubernetes.
 provider "kubernetes" {}
 
 # O provedor do Helm.
@@ -7,12 +6,23 @@ provider "helm" {
   kubernetes = {}
 }
 
+# Cria o namespace para o Grafana
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
   }
 }
 
+# --- LINHA ADICIONADA AQUI ---
+# Cria o namespace para a aplicação
+resource "kubernetes_namespace" "production" {
+  metadata {
+    name = "production"
+  }
+}
+# -----------------------------
+
+# Instala o Grafana
 resource "helm_release" "grafana" {
   name       = "grafana-dashboard"
   repository = "https://grafana.github.io/helm-charts"
