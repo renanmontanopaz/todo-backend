566f235e1cc1b663fe91049690c63c728d50742d
renan <renanmontanopaz@outlook.com>
2025-06-20 16:21:44 -0300

    pipeline

--- infra/main.tf
+++ infra/main.tf
@@ -4,20 +4,20 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_container_cluster" "primary" {
-  name     = "gke-standard-cluster"
-  location = "us-central1"
-
-  # Remove o pool de nós padrão para criarmos um customizado
-  remove_default_node_pool = true
-  initial_node_count       = 3
-}
+# resource "google_container_cluster" "primary" {
+#   name     = "gke-standard-cluster"
+#   location = "us-central1"
+#
+#   # Remove o pool de nós padrão para criarmos um customizado
+#   remove_default_node_pool = true
+#   initial_node_count       = 3
+# }
 
 resource "google_container_node_pool" "primary_nodes" {
   name       = "default-pool"
   cluster    = google_container_cluster.primary.name
   location   = google_container_cluster.primary.location
-  node_count = 1 # Começamos com 1 nó, mas podemos habilitar o autoscaling
+  node_count = 3 # Começamos com 1 nó, mas podemos habilitar o autoscaling
 
   node_config {
     # e2-medium é uma máquina boa e de custo-benefício para começar
