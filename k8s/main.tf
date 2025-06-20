99f3e343700b69e877424be923ed0c591d0f68d4
renan <renanmontanopaz@outlook.com>
2025-06-20 17:06:14 -0300

    pipeline

--- k8s/main.tf
+++ k8s/main.tf
@@ -1,34 +1,34 @@
-terraform {
-  backend "gcs" {
-    bucket = "tfstate-gke-standard-renanmphp" # Use o mesmo nome do bucket que você criou
-    prefix = "standard-infra"             # Pasta para o estado da infraestrutura
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = "tfstate-gke-standard-renanmphp" # Use o mesmo nome do bucket que você criou
+#     prefix = "standard-infra"             # Pasta para o estado da infraestrutura
+#   }
+# }
 # Provedores Kubernetes e Helm (a configuração agora é inferida pelo ambiente da pipeline)
 provider "kubernetes" {}
 provider "helm" {}
 
 # Cria o namespace para a aplicação
-resource "kubernetes_namespace" "production" {
-  metadata {
-    name = "production"
-  }
-}
-
-# Cria o namespace para o monitoramento
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    name = "monitoring"
-  }
-}
-
-# Instala a PILHA COMPLETA de monitoramento
-resource "helm_release" "prometheus_stack" {
-  name       = "prometheus-stack"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
-
-  # Garante que a instalação do Helm só ocorrerá após a criação do namespace
-  depends_on = [kubernetes_namespace.monitoring]
-}
+# resource "kubernetes_namespace" "production" {
+#   metadata {
+#     name = "production"
+#   }
+# }
+#
+# # Cria o namespace para o monitoramento
+# resource "kubernetes_namespace" "monitoring" {
+#   metadata {
+#     name = "monitoring"
+#   }
+# }
+#
+# # Instala a PILHA COMPLETA de monitoramento
+# resource "helm_release" "prometheus_stack" {
+#   name       = "prometheus-stack"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#   namespace  = kubernetes_namespace.monitoring.metadata[0].name
+#
+#   # Garante que a instalação do Helm só ocorrerá após a criação do namespace
+#   depends_on = [kubernetes_namespace.monitoring]
+# }
