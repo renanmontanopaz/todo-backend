8414e98d0c7f727f65c3fb177383172de7d44003
renan <renanmontanopaz@outlook.com>
2025-06-20 18:54:08 -0300

    pipeline

--- k8s/main.tf
+++ k8s/main.tf
@@ -9,26 +9,24 @@ provider "kubernetes" {}
 provider "helm" {}
 
 # Cria o namespace para a aplicação
-# resource "kubernetes_namespace" "production" {
-#   metadata {
-#     name = "production"
-#   }
-# }
+resource "kubernetes_namespace" "production" {
+  metadata {
+    name = "production"
+  }
+}
 #
 # # Cria o namespace para o monitoramento
-# resource "kubernetes_namespace" "monitoring" {
-#   metadata {
-#     name = "monitoring"
-#   }
-# }
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
 #
-# # Instala a PILHA COMPLETA de monitoramento
-# resource "helm_release" "prometheus_stack" {
-#   name       = "prometheus-stack"
-#   repository = "https://prometheus-community.github.io/helm-charts"
-#   chart      = "kube-prometheus-stack"
-#   namespace  = kubernetes_namespace.monitoring.metadata[0].name
-#
-#   # Garante que a instalação do Helm só ocorrerá após a criação do namespace
-#   depends_on = [kubernetes_namespace.monitoring]
-# }
+resource "helm_release" "grafana" {
+  name       = "grafana-dashboard"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
+  depends_on = [kubernetes_namespace.monitoring]
+}
